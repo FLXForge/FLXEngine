cc8401bdd7ca6828f336abbb257f88124dd90147
RafaelOcasar <51993344+RafaelOcasar@users.noreply.github.com>
2026-09-17 18:05:12 +0200

    audit example arkanoid

---
+++ examples/arkanoid/level.js
@@ -0,0 +1,6 @@
+/// <reference path="https://flxforge.github.io/FLXEngine/scripts/flx.d.ts" />
+
+function born(level){
+    fade_set(1);
+    fade_off();
+}
--- examples/arkanoid/maintitle/paddle.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="https://flxforge.github.io/FLXEngine/scripts/flx.d.ts" />
-
-function motion(paddle) {
-    const balls = find_name("title_ball");
-
-    if (balls.length > 0) {
-        follow_x(paddle, balls[0]);
-    }
-}

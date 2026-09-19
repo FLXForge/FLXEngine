76de24f3aeb1dbb0a190de74b0a0ef52f18af7a0
RafaelOcasar <51993344+RafaelOcasar@users.noreply.github.com>
2026-09-19 16:03:18 +0200

    audit examples arkanoid

--- examples/arkanoid/level/paddle.js
+++ examples/arkanoid/level/paddle.js
@@ -6,91 +6,53 @@
 */
 
 const POWERUP_TIME = 5;
-
 const BIG_FACTOR = 1.5;
 
-const RESIZE_SPEED = 180;
+let normalWidth;
 
-let normalWidth = 70;
-let targetWidth = 70;
-
-function born(paddle){
+function born(paddle) {
     normalWidth = paddle.width;
-    targetWidth = normalWidth;
 }
 
-function action(paddle){
-    update_target_width(paddle);
-    update_resize(paddle);
-    update_movement(paddle);
-    update_respawn(paddle);
-}
-
-function update_target_width(paddle){
-    if (timer_active(paddle, "big")) {
-        targetWidth = normalWidth * BIG_FACTOR;
-        return;
-    }
-
-    targetWidth = normalWidth;
-}
-
-function update_resize(paddle){
-    if (paddle.width < targetWidth) {
-        resize_width(paddle, paddle.width + RESIZE_SPEED * delta());
-
-        if (paddle.width > targetWidth) {
-            resize_width(paddle, targetWidth);
-        }
-    }
-
-    if (paddle.width > targetWidth) {
-        resize_width(paddle, paddle.width - RESIZE_SPEED * delta());
-
-        if (paddle.width < targetWidth) {
-            resize_width(paddle, targetWidth);
-        }
-    }
-}
-
-function update_movement(paddle){
+function action(paddle) {
     const MOVE = direction(0);
-    move_horizontal(paddle, input_direction(paddle, MOVE, HORIZONTAL));
-}
 
-function update_respawn(paddle){
+    move_horizontal(
+        paddle,
+        input_direction(paddle, MOVE, HORIZONTAL)
+    );
+
     if (state_active(paddle, "respawn")) {
         spawn(paddle, "ball");
         play_sound(paddle, "pop");
         state_to(paddle, "playing");
     }
+
+    if (timer_done(paddle, "big")) {
+        resize_width(paddle, normalWidth);
+        stop_timer(paddle, "big");
+    }
 }
 
-function collision(paddle, other) {
+function collision(paddle, other, contacts) {
     if (other.group == "wall") {
-        if (paddle.x < other.x) {
-            position_x(paddle, other.x - other.width / 2 - paddle.width / 2);
-        } else {
-            position_x(paddle, other.x + other.width / 2 + paddle.width / 2);
+        for (const contact of contacts) {
+            position(paddle, contact);
         }
 
         return;
     }
 
     if (other.group == "powerup") {
-        apply_powerup(paddle, other);
+        if (other.name == "glue") {
+            play_timer(paddle, "glue", POWERUP_TIME);
+        }
+
+        if (other.name == "big") {
+            resize_width(paddle, normalWidth * BIG_FACTOR);
+            play_timer(paddle, "big", POWERUP_TIME);
+        }
+
         kill(other);
-    }
-}
-
-function apply_powerup(paddle, powerup){
-    if (powerup.name == "glue") {
-        stop_timer(paddle, "glue");
-        play_timer(paddle, "glue", POWERUP_TIME);
-    }
-
-    if (powerup.name == "big") {
-        stop_timer(paddle, "big");
-        play_timer(paddle, "big", POWERUP_TIME);
     }
 }

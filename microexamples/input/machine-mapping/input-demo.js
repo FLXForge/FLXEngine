b3ca80015aca77fab247ece9f34e20daa99d84ee
RafaelOcasar <51993344+RafaelOcasar@users.noreply.github.com>
2026-09-21 15:56:06 +0200

    audit microexamples

--- microexamples/input/machine-mapping/input-demo.js
+++ microexamples/input/machine-mapping/input-demo.js
@@ -14,3 +14,9 @@ function action(object) {
         input_down(object, ACTION) ? 1 : 0
     );
 }
+
+function draw(object) {
+    draw_text(object, 100,  20, "Horizontal: " + read_local(object, "horizontal"), 20);
+    draw_text(object, 100,  40, "Vertical: " + read_local(object, "vertical"), 20);
+    draw_text(object, 100,  60, "Action: " + read_local(object, "action"), 20);
+}
